4c942e4800a556332be49687ffcab30448d2bd91
unknown <chez90@mail.ru>
2019-06-09 21:08:16 +0400

    create component "travel", begin to create html-layout

---
+++ src/components/travel.vue
@@ -0,0 +1,46 @@
+<template>
+	<div>
+		<header>
+			<div class="header">
+				<img src="../assets/logo.png" alt="Travel" class="logo">
+				<p class="headMessage">Друзья, мои походы пока еще <br> не закончились, делюсь с вами!</p>
+			</div>
+		</header>
+
+		<section>
+
+		</section>
+
+		<footer>
+
+		</footer>
+	</div>
+</template>
+
+<script>
+export default {
+	
+}
+</script>
+
+<style>
+	body {
+		margin: 0;
+		padding:0;
+	}
+	.header {
+		background: url(../assets/head.png) no-repeat center top ;
+		height: 302px;
+		
+	}
+	.logo {
+		padding-left: 228px;
+		padding-top: 48px;
+	}
+	.headMessage {
+		font-size: 40px;
+		color: #ffffff;
+		padding-left: 720px;
+		margin-top: 0px;
+	}
+</style>
