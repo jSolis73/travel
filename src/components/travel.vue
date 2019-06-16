6550efcc31e06f55c9bf4ce79917397e40196f48
unknown <chez90@mail.ru>
2019-06-16 20:30:46 +0400

    add method, which delete post in database

--- src/components/travel.vue
+++ src/components/travel.vue
@@ -3,15 +3,15 @@
 		<header-app />
 		<section>
 			<div class="content" v-for="(post, id) in posts" :key="id">
-				<div class="displayPosts">
-						<v-icon class ="icon">close</v-icon>
+				<div class="displayPosts" >
+						<v-icon class ="icon" @click="deletePost(post.id)">close</v-icon>
 					<div class="wrapper">
-						<div class="realTime">{{post.realTime}}</div>
-						<div class="title">{{post.title}}</div> 
+						<div class="realTime">{{post.data().realTime}}</div>
+						<div class="title">{{post.data().title}}</div> 
 					</div>
 					<div class="wrapper">
-						<div class="place">{{post.place}}</div>
-						<div class="message">{{post.message}}</div>
+						<div class="place">{{post.data().place}}</div>
+						<div class="message">{{post.data().message}}</div>
 					</div>
 				</div>
 			</div>	
@@ -42,12 +42,25 @@ export default {
     .then((querySnapshot) => {
         querySnapshot.forEach((doc) => {
             // doc.data() is never undefined for query doc snapshots
-            this.posts.unshift(doc.data());
+            this.posts.unshift(doc);
         });
     })
     .catch(function(error) {
         console.log("Error getting documents: ", error);
     });			
+		},
+		deletePost(doc) {
+			if (confirm("Вы точно хотите удалить пост?"))
+				{
+					db.collection("posts").doc(doc).delete().then(() => {
+					console.log("Document successfully deleted!");
+					}).catch((error) => {
+							console.error("Error removing document: ", error);
+					});
+				}
+			else {
+
+			}	
 		}
 	},
 	created() {
